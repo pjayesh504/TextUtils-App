9fef6aeace7f014ce0c96d0dc2f8aab6b95ceedf
Jayesh504 <pjayesh504@gmail.com>
2022-11-30 12:24:08 +0530

    Updates

---
+++ precache-manifest.25f97da8caae56024ab47548aaa2f37e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b9f7c69a7f95beb9abb5",
+    "url": "/TextUtils-App/static/css/main.9be270f3.chunk.css"
+  },
+  {
+    "revision": "b9f7c69a7f95beb9abb5",
+    "url": "/TextUtils-App/static/js/main.b9f7c69a.chunk.js"
+  },
+  {
+    "revision": "95ea83b6c7492a61ef7b",
+    "url": "/TextUtils-App/static/js/1.95ea83b6.chunk.js"
+  },
+  {
+    "revision": "31d77044af729c125605",
+    "url": "/TextUtils-App/static/js/2.31d77044.chunk.js"
+  },
+  {
+    "revision": "431d9bdf2d1c9e2549b1",
+    "url": "/TextUtils-App/static/js/runtime~main.431d9bdf.js"
+  },
+  {
+    "revision": "b632537dbddff52ea053c8bcfdde8689",
+    "url": "/TextUtils-App/index.html"
+  }
+];
